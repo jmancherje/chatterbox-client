011f74427b63f4c851fb6bf7558f4bb208c36c6c
jmancherje <mancherje.justin@gmail.com>
2015-12-07 15:37:15 -0800

    tests passing thru fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,45 @@
 // YOUR CODE HERE:
+
+var app = {
+  init : function(){$.get('https://api.parse.com/1/classes/chatterbox.jsonp', message)}
+};
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.send = function (message) { 
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    dataType: 'jsonp',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('Chatterbox: message sent');
+      console.log(data);
+    },
+    error: function (data) {
+      throw 'chatterbox: Failed to send message';
+    }
+  });
+};
+
+app.fetch = function (){
+    $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: JSON.stringify(message),
+    dataType: 'jsonp',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('Chatterbox: message sent');
+      console.log(data);
+    },
+    error: function (data) {
+      throw 'chatterbox: Failed to send message';
+    }
+  });
+};
