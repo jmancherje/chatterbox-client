f6b64f3310ff17f066ee54c3239fe96fa4d96993
jmancherje <mancherje.justin@gmail.com>
2015-12-07 16:21:22 -0800

    app.fetch posts to chatterbox. Add messages.css to style new messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,29 +1,33 @@
 // YOUR CODE HERE:
 
 var app = {
-  init : function(){$.get('https://api.parse.com/1/classes/chatterbox.jsonp', message)}
+  init : function () {
+    //$.get('https://api.parse.com/1/classes/chatterbox.json', message)
+  }
 };
 
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
 app.server = 'https://api.parse.com/1/classes/chatterbox';
+var messageStore = [];
 app.send = function (message) { 
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'POST',
     data: JSON.stringify(message),
-    dataType: 'jsonp',
+    dataType: 'json',
     contentType: 'application/json',
     success: function (data) {
       console.log('Chatterbox: message sent');
-      console.log(data);
     },
     error: function (data) {
       throw 'chatterbox: Failed to send message';
     }
+
   });
 };
 
@@ -32,14 +36,23 @@ app.fetch = function (){
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     data: JSON.stringify(message),
-    dataType: 'jsonp',
+    dataType: 'json',
     contentType: 'application/json',
     success: function (data) {
       console.log('Chatterbox: message sent');
       console.log(data);
+      var recentMessage = data.results[0]
+        
+      $('#chats').append('<div class="chatMessage"> <p class="userName">' 
+        + recentMessage.username  + '</p> <p class="messageContent">' 
+        + recentMessage.text + '</p> </div>');
     },
     error: function (data) {
       throw 'chatterbox: Failed to send message';
     }
   });
+};
+
+app.clearMessages = function (){
+  // debugger;
 };
